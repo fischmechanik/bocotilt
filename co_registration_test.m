c943dc6da4aeec6ac25002107d22837ed4ef0765
plkn <fischmechanik@gmail.com>
2021-09-27 14:56:35 +0200

    subject 5

--- co_registration_test.m
+++ co_registration_test.m
@@ -3,13 +3,17 @@ clear all;
 % Paths...
 PATH_FIELDTRIP = '/home/plkn/fieldtrip-master/';
 PATH_EEGCAP_MODELS = '/mnt/data_dump/bocotilt/0_eegcap_models/';
+PATH_WORKING_DIRECTORY = '/home/plkn/Desktop/elecpos_practice/'; 
 
 % Init eeglab
 addpath(PATH_FIELDTRIP);
 ft_defaults;
 
+% Set subject
+subject = 'VP04';
+
 % Load a headshape
-head_surface = ft_read_headshape([PATH_EEGCAP_MODELS, 'VP03_model.obj']);
+head_surface = ft_read_headshape([PATH_EEGCAP_MODELS, subject, '/Model.obj']);
 
 % Convert units to mm
 head_surface = ft_convert_units(head_surface, 'mm');
@@ -21,6 +25,7 @@ head_surface = ft_convert_units(head_surface, 'mm');
 cfg = [];
 cfg.method = 'headshape';
 fiducials = ft_electrodeplacement(cfg, head_surface);
+
 
 % Convert to CTF-coordinates using feducials
 cfg = []
@@ -34,5 +39,19 @@ head_surface = ft_meshrealign(cfg, head_surface);
 % Plot CFT axes
 ft_plot_axes(head_surface);
 ft_plot_mesh(head_surface);
+
+% Save
+save([PATH_WORKING_DIRECTORY, 'head_surface_fiducials_added.mat'], 'head_surface');
+
+% Load
+load([PATH_WORKING_DIRECTORY, 'head_surface_fiducials_added.mat']);
+
+% Identify electrode positions
+cfg = [];
+cfg.method = 'headshape';
+cfg.channel = {'1'};
+elec = ft_electrodeplacement(cfg, head_surface);
+
+
 
 
