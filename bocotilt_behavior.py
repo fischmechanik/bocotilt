d94105d6a21f54c8952b55a50ad11aefed2c8aab
plkn <fischmechanik@gmail.com>
2021-11-18 16:32:11 +0100

    behavioral analysis adjusted

--- bocotilt_behavior.py
+++ bocotilt_behavior.py
@@ -41,104 +41,55 @@ dat = data[0]
 # 18: sequence_position
 # 19: sequence_length
 
-# Get RTs
+# Get RT
 bon_pos1_mean = np.nanmean(
-    dat[(dat[:, 3] == 1) & (dat[:, 18] == 1) & (dat[:, 1] > 4), 12]
+    dat[(dat[:, 3] == 1) & (dat[:, 18] == 1) & (dat[:, 1] > 4) & (dat[:, 13] == 1), 12]
 )
 std_pos1_mean = np.nanmean(
-    dat[(dat[:, 3] == 0) & (dat[:, 18] == 1) & (dat[:, 1] > 4), 12]
+    dat[(dat[:, 3] == 0) & (dat[:, 18] == 1) & (dat[:, 1] > 4) & (dat[:, 13] == 1), 12]
 )
 bon_pos2_mean = np.nanmean(
-    dat[(dat[:, 3] == 1) & (dat[:, 18] == 2) & (dat[:, 1] > 4), 12]
+    dat[(dat[:, 3] == 1) & (dat[:, 18] == 2) & (dat[:, 1] > 4) & (dat[:, 13] == 1), 12]
 )
 std_pos2_mean = np.nanmean(
-    dat[(dat[:, 3] == 0) & (dat[:, 18] == 2) & (dat[:, 1] > 4), 12]
+    dat[(dat[:, 3] == 0) & (dat[:, 18] == 2) & (dat[:, 1] > 4) & (dat[:, 13] == 1), 12]
 )
 bon_pos3_mean = np.nanmean(
-    dat[(dat[:, 3] == 1) & (dat[:, 18] == 3) & (dat[:, 1] > 4), 12]
+    dat[(dat[:, 3] == 1) & (dat[:, 18] == 3) & (dat[:, 1] > 4) & (dat[:, 13] == 1), 12]
 )
 std_pos3_mean = np.nanmean(
-    dat[(dat[:, 3] == 0) & (dat[:, 18] == 3) & (dat[:, 1] > 4), 12]
+    dat[(dat[:, 3] == 0) & (dat[:, 18] == 3) & (dat[:, 1] > 4) & (dat[:, 13] == 1), 12]
 )
 bon_pos4_mean = np.nanmean(
-    dat[(dat[:, 3] == 1) & (dat[:, 18] == 4) & (dat[:, 1] > 4), 12]
+    dat[(dat[:, 3] == 1) & (dat[:, 18] == 4) & (dat[:, 1] > 4) & (dat[:, 13] == 1), 12]
 )
 std_pos4_mean = np.nanmean(
-    dat[(dat[:, 3] == 0) & (dat[:, 18] == 4) & (dat[:, 1] > 4), 12]
+    dat[(dat[:, 3] == 0) & (dat[:, 18] == 4) & (dat[:, 1] > 4) & (dat[:, 13] == 1), 12]
 )
 bon_pos5_mean = np.nanmean(
-    dat[(dat[:, 3] == 1) & (dat[:, 18] == 5) & (dat[:, 1] > 4), 12]
+    dat[(dat[:, 3] == 1) & (dat[:, 18] == 5) & (dat[:, 1] > 4) & (dat[:, 13] == 1), 12]
 )
 std_pos5_mean = np.nanmean(
-    dat[(dat[:, 3] == 0) & (dat[:, 18] == 5) & (dat[:, 1] > 4), 12]
+    dat[(dat[:, 3] == 0) & (dat[:, 18] == 5) & (dat[:, 1] > 4) & (dat[:, 13] == 1), 12]
 )
 bon_pos6_mean = np.nanmean(
-    dat[(dat[:, 3] == 1) & (dat[:, 18] == 6) & (dat[:, 1] > 4), 12]
+    dat[(dat[:, 3] == 1) & (dat[:, 18] == 6) & (dat[:, 1] > 4) & (dat[:, 13] == 1), 12]
 )
 std_pos6_mean = np.nanmean(
-    dat[(dat[:, 3] == 0) & (dat[:, 18] == 6) & (dat[:, 1] > 4), 12]
+    dat[(dat[:, 3] == 0) & (dat[:, 18] == 6) & (dat[:, 1] > 4) & (dat[:, 13] == 1), 12]
 )
 bon_pos7_mean = np.nanmean(
-    dat[(dat[:, 3] == 1) & (dat[:, 18] == 7) & (dat[:, 1] > 4), 12]
+    dat[(dat[:, 3] == 1) & (dat[:, 18] == 7) & (dat[:, 1] > 4) & (dat[:, 13] == 1), 12]
 )
 std_pos7_mean = np.nanmean(
-    dat[(dat[:, 3] == 0) & (dat[:, 18] == 7) & (dat[:, 1] > 4), 12]
+    dat[(dat[:, 3] == 0) & (dat[:, 18] == 7) & (dat[:, 1] > 4) & (dat[:, 13] == 1), 12]
 )
 bon_pos8_mean = np.nanmean(
-    dat[(dat[:, 3] == 1) & (dat[:, 18] == 8) & (dat[:, 1] > 4), 12]
+    dat[(dat[:, 3] == 1) & (dat[:, 18] == 8) & (dat[:, 1] > 4) & (dat[:, 13] == 1), 12]
 )
 std_pos8_mean = np.nanmean(
-    dat[(dat[:, 3] == 0) & (dat[:, 18] == 8) & (dat[:, 1] > 4), 12]
+    dat[(dat[:, 3] == 0) & (dat[:, 18] == 8) & (dat[:, 1] > 4) & (dat[:, 13] == 1), 12]
 )
-bon_pos1_error = np.nanstd(
-    dat[(dat[:, 3] == 1) & (dat[:, 18] == 1) & (dat[:, 1] > 4), 12]
-)
-std_pos1_error = np.nanstd(
-    dat[(dat[:, 3] == 0) & (dat[:, 18] == 1) & (dat[:, 1] > 4), 12]
-)
-bon_pos2_error = np.nanstd(
-    dat[(dat[:, 3] == 1) & (dat[:, 18] == 2) & (dat[:, 1] > 4), 12]
-)
-std_pos2_error = np.nanstd(
-    dat[(dat[:, 3] == 0) & (dat[:, 18] == 2) & (dat[:, 1] > 4), 12]
-)
-bon_pos3_error = np.nanstd(
-    dat[(dat[:, 3] == 1) & (dat[:, 18] == 3) & (dat[:, 1] > 4), 12]
-)
-std_pos3_error = np.nanstd(
-    dat[(dat[:, 3] == 0) & (dat[:, 18] == 3) & (dat[:, 1] > 4), 12]
-)
-bon_pos4_error = np.nanstd(
-    dat[(dat[:, 3] == 1) & (dat[:, 18] == 4) & (dat[:, 1] > 4), 12]
-)
-std_pos4_error = np.nanstd(
-    dat[(dat[:, 3] == 0) & (dat[:, 18] == 4) & (dat[:, 1] > 4), 12]
-)
-bon_pos5_error = np.nanstd(
-    dat[(dat[:, 3] == 1) & (dat[:, 18] == 5) & (dat[:, 1] > 4), 12]
-)
-std_pos5_error = np.nanstd(
-    dat[(dat[:, 3] == 0) & (dat[:, 18] == 5) & (dat[:, 1] > 4), 12]
-)
-bon_pos6_error = np.nanstd(
-    dat[(dat[:, 3] == 1) & (dat[:, 18] == 6) & (dat[:, 1] > 4), 12]
-)
-std_pos6_error = np.nanstd(
-    dat[(dat[:, 3] == 0) & (dat[:, 18] == 6) & (dat[:, 1] > 4), 12]
-)
-bon_pos7_error = np.nanstd(
-    dat[(dat[:, 3] == 1) & (dat[:, 18] == 7) & (dat[:, 1] > 4), 12]
-)
-std_pos7_error = np.nanstd(
-    dat[(dat[:, 3] == 0) & (dat[:, 18] == 7) & (dat[:, 1] > 4), 12]
-)
-bon_pos8_error = np.nanstd(
-    dat[(dat[:, 3] == 1) & (dat[:, 18] == 8) & (dat[:, 1] > 4), 12]
-)
-std_pos8_error = np.nanstd(
-    dat[(dat[:, 3] == 0) & (dat[:, 18] == 8) & (dat[:, 1] > 4), 12]
-)
-
 
 # Arange plotting data
 condition_labels = [
@@ -173,45 +124,112 @@ std_means = [
     std_pos7_mean,
     std_pos8_mean,
 ]
-bon_error = [
-    bon_pos1_error,
-    bon_pos2_error,
-    bon_pos3_error,
-    bon_pos4_error,
-    bon_pos5_error,
-    bon_pos6_error,
-    bon_pos7_error,
-    bon_pos8_error,
-]
-std_error = [
-    std_pos1_error,
-    std_pos2_error,
-    std_pos3_error,
-    std_pos4_error,
-    std_pos5_error,
-    std_pos6_error,
-    std_pos7_error,
-    std_pos8_error,
-]
 
 
 # Build the plot
-fig, ax = plt.subplots()
+fig, ax = plt.subplots(2, 1)
+rects1 = ax[0].bar(
+    x_pos - barwidth / 2, std_means, barwidth, label="standard", color="olive"
+)
+rects2 = ax[0].bar(
+    x_pos + barwidth / 2, bon_means, barwidth, label="bonus", color="salmon"
+)
+ax[0].set_ylabel("ms")
+ax[0].set_ylim((500, 900))
+ax[0].set_xticks(x_pos)
+ax[0].set_xticklabels(condition_labels)
+ax[0].set_title("RT")
+ax[0].yaxis.grid(True)
+ax[0].legend()
 
-rects1 = ax.bar(x_pos - barwidth / 2, std_means, barwidth, label="standard")
-rects2 = ax.bar(x_pos + barwidth / 2, bon_means, barwidth, label="bonus")
+# Get accuracy
+bon_pos1_acc = sum(
+    dat[(dat[:, 3] == 1) & (dat[:, 18] == 1) & (dat[:, 1] > 4), 13] == 1
+) / len(dat[(dat[:, 3] == 1) & (dat[:, 18] == 1) & (dat[:, 1] > 4), 13])
+std_pos1_acc = sum(
+    dat[(dat[:, 3] == 0) & (dat[:, 18] == 1) & (dat[:, 1] > 4), 13] == 1
+) / len(dat[(dat[:, 3] == 0) & (dat[:, 18] == 1) & (dat[:, 1] > 4), 13])
+bon_pos2_acc = sum(
+    dat[(dat[:, 3] == 1) & (dat[:, 18] == 2) & (dat[:, 1] > 4), 13] == 1
+) / len(dat[(dat[:, 3] == 1) & (dat[:, 18] == 2) & (dat[:, 1] > 4), 13])
+std_pos2_acc = sum(
+    dat[(dat[:, 3] == 0) & (dat[:, 18] == 2) & (dat[:, 1] > 4), 13] == 1
+) / len(dat[(dat[:, 3] == 0) & (dat[:, 18] == 2) & (dat[:, 1] > 4), 13])
+bon_pos3_acc = sum(
+    dat[(dat[:, 3] == 1) & (dat[:, 18] == 3) & (dat[:, 1] > 4), 13] == 1
+) / len(dat[(dat[:, 3] == 1) & (dat[:, 18] == 3) & (dat[:, 1] > 4), 13])
+std_pos3_acc = sum(
+    dat[(dat[:, 3] == 0) & (dat[:, 18] == 3) & (dat[:, 1] > 4), 13] == 1
+) / len(dat[(dat[:, 3] == 0) & (dat[:, 18] == 3) & (dat[:, 1] > 4), 13])
+bon_pos4_acc = sum(
+    dat[(dat[:, 3] == 1) & (dat[:, 18] == 4) & (dat[:, 1] > 4), 13] == 1
+) / len(dat[(dat[:, 3] == 1) & (dat[:, 18] == 4) & (dat[:, 1] > 4), 13])
+std_pos4_acc = sum(
+    dat[(dat[:, 3] == 0) & (dat[:, 18] == 4) & (dat[:, 1] > 4), 13] == 1
+) / len(dat[(dat[:, 3] == 0) & (dat[:, 18] == 4) & (dat[:, 1] > 4), 13])
+bon_pos5_acc = sum(
+    dat[(dat[:, 3] == 1) & (dat[:, 18] == 5) & (dat[:, 1] > 4), 13] == 1
+) / len(dat[(dat[:, 3] == 1) & (dat[:, 18] == 5) & (dat[:, 1] > 4), 13])
+std_pos5_acc = sum(
+    dat[(dat[:, 3] == 0) & (dat[:, 18] == 5) & (dat[:, 1] > 4), 13] == 1
+) / len(dat[(dat[:, 3] == 0) & (dat[:, 18] == 5) & (dat[:, 1] > 4), 13])
+bon_pos6_acc = sum(
+    dat[(dat[:, 3] == 1) & (dat[:, 18] == 6) & (dat[:, 1] > 4), 13] == 1
+) / len(dat[(dat[:, 3] == 1) & (dat[:, 18] == 6) & (dat[:, 1] > 4), 13])
+std_pos6_acc = sum(
+    dat[(dat[:, 3] == 0) & (dat[:, 18] == 6) & (dat[:, 1] > 4), 13] == 1
+) / len(dat[(dat[:, 3] == 0) & (dat[:, 18] == 6) & (dat[:, 1] > 4), 13])
+bon_pos7_acc = sum(
+    dat[(dat[:, 3] == 1) & (dat[:, 18] == 7) & (dat[:, 1] > 4), 13] == 1
+) / len(dat[(dat[:, 3] == 1) & (dat[:, 18] == 7) & (dat[:, 1] > 4), 13])
+std_pos7_acc = sum(
+    dat[(dat[:, 3] == 0) & (dat[:, 18] == 7) & (dat[:, 1] > 4), 13] == 1
+) / len(dat[(dat[:, 3] == 0) & (dat[:, 18] == 7) & (dat[:, 1] > 4), 13])
+bon_pos8_acc = sum(
+    dat[(dat[:, 3] == 1) & (dat[:, 18] == 8) & (dat[:, 1] > 4), 13] == 1
+) / len(dat[(dat[:, 3] == 1) & (dat[:, 18] == 8) & (dat[:, 1] > 4), 13])
+std_pos8_acc = sum(
+    dat[(dat[:, 3] == 0) & (dat[:, 18] == 8) & (dat[:, 1] > 4), 13] == 1
+) / len(dat[(dat[:, 3] == 0) & (dat[:, 18] == 8) & (dat[:, 1] > 4), 13])
 
-ax.set_ylabel("ms")
-ax.set_xticks(x_pos)
-ax.set_xticklabels(condition_labels)
-ax.set_title("mean RT standard vs bonus trials")
-ax.yaxis.grid(True)
-ax.legend()
+# Arange plotting data
+barwidth = 0.35
+bon_acc = [
+    bon_pos1_acc,
+    bon_pos2_acc,
+    bon_pos3_acc,
+    bon_pos4_acc,
+    bon_pos5_acc,
+    bon_pos6_acc,
+    bon_pos7_acc,
+    bon_pos8_acc,
+]
+std_acc = [
+    std_pos1_acc,
+    std_pos2_acc,
+    std_pos3_acc,
+    std_pos4_acc,
+    std_pos5_acc,
+    std_pos6_acc,
+    std_pos7_acc,
+    std_pos8_acc,
+]
 
-# ax.bar_label(rects1, padding=3)
-# ax.bar_label(rects2, padding=3)
+# Build the plot
+rects1 = ax[1].bar(
+    x_pos - barwidth / 2, std_acc, barwidth, label="standard", color="olive"
+)
+rects2 = ax[1].bar(
+    x_pos + barwidth / 2, bon_acc, barwidth, label="bonus", color="salmon"
+)
+ax[1].set_ylabel("% correct")
+ax[1].set_ylim((0.5, 0.9))
+ax[1].set_xticks(x_pos)
+ax[1].set_xticklabels(condition_labels)
+ax[1].set_title("accuracy")
+ax[1].yaxis.grid(True)
+
 
 fig.tight_layout()
-
 plt.show()
 
