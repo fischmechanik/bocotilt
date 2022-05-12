787a106f11d0b025fc8f9c323889d9c22f7b4362
plkn <fischmechanik@gmail.com>
2022-05-12 19:57:43 +0200

    theta parameterization

--- bocotilt_behavior.py
+++ bocotilt_behavior.py
@@ -3,10 +3,14 @@ import glob
 import numpy as np
 import pandas as pd
 import seaborn as sns
+import scipy.io
 import statsmodels.stats.anova
+import statsmodels.formula.api as smf
+import os
 
 # Paths
 path_in = "/mnt/data_dump/bocotilt/2_autocleaned/"
+path_theta = "/mnt/data_dump/bocotilt/7_ged/"
 
 # Get datasets
 datasets = glob.glob(f"{path_in}/*_trialinfo.csv")
@@ -14,7 +18,10 @@ datasets = glob.glob(f"{path_in}/*_trialinfo.csv")
 # Read datasets
 data = []
 for dataset_idx, dataset in enumerate(datasets):
-    data.append(np.genfromtxt(dataset, delimiter=","))
+    
+    # Skip excluded
+    if not (int(dataset[-16:-14]) in [10, 28]):
+        data.append(np.genfromtxt(dataset, delimiter=","))
 
 # Stack data
 data = np.vstack(data)
@@ -52,21 +59,32 @@ data[data[:, 21] == 1, 9] = -1
 
 # Add binarized sequential positions
 binarized_seqpos = np.zeros((data.shape[0], 1)) - 1
-binarized_seqpos[data[:, 21] <= 3, 0] = 0
-binarized_seqpos[data[:, 21] >= 7, 0] = 1
+binarized_seqpos[data[:, 21] <= 4, 0] = 0
+binarized_seqpos[data[:, 21] >= 5, 0] = 1
 data = np.hstack((data, binarized_seqpos))
+
+# Add binarized block positions
+binarized_blockpos = np.zeros((data.shape[0], 1)) - 1
+binarized_blockpos[data[:, 1] <= 7, 0] = 0
+binarized_blockpos[data[:, 1] >= 8, 0] = 1
+data = np.hstack((data, binarized_blockpos))
+
 
 # 23 seqpos_binarized
 
 # Remove non defined
 data = data[data[:, 16] != 2, :] # Remove missing resposes
-#data = data[data[:, 9] != -1, :] # Remove non-defined sequences
-data = data[data[:, 1] > 4, :] # Remove practice blocks
+data = data[data[:, 1] > 4, :]  # Remove practice blocks
+
+
+data = data[data[:, 9] != -1, :] # Remove non-defined sequences
+
 #data = data[data[:, 23] >= 0, :] # Remove trials not belonging to bnarized sequence ranges
+data = data[data[:, 24] >= 0, :] # Remove trials not belonging to bnarized block ranges
 
 # Seperate datasets for rt and accuracy analyses
-data_rt = data[data[:, 16] == 1, :] # Remove incorrect trials
-data_acc = data
+data_rt = data[data[:, 16] == 1, :]  # Remove incorrect trials
+
 
 # Define columns
 columns = [
@@ -94,100 +112,231 @@ columns = [
     "sequence_position",
     "sequence_length",
     "seqpos_binarized",
+    "blockpos_binarized",
 ]
 
 # ==============================================================================================================
 
-# Create rt df
-df_rt = pd.DataFrame(data=data_rt, columns=columns)
+# Create df
+df_root = pd.DataFrame(data=data, columns=columns)
 
-# Draw individual rt
-g = sns.catplot(
-    x="bonustrial",
-    y="log_rt",
-    hue="id",
-    col="task_switch",
-    capsize=0.05,
-    palette="tab20",
-    height=6,
-    aspect=0.75,
-    kind="point",
-    data=df_rt,
-)
-g.despine(left=True)
+# Select variables to consider
+factors = ["block_nr", "bonustrial", "task_switch"]
 
-# Sequential_position x bonus_trial for rt
-df_anova_rt = df_rt.groupby(
-     ["id", "bonustrial",  "sequence_position"], as_index=False
+# Calculate rates of correct responses, incorrect responses and omissions
+df_rates = df_root.groupby(
+    ["id"] + factors, as_index=False
+).log_accuracy.value_counts(normalize=True)
+
+# Create a dara frame for response times using only correct responses
+df_behavior = pd.DataFrame(data=data[data[:, 16] == 1, :], columns=columns)
+
+# Calculate rates of correct responses, incorrect responses and omissions
+df_behavior_grouped = df_behavior.groupby(
+   ["id"] + factors, as_index=False
 )["log_rt"].mean()
 
+# Iterate rows
+incorrect = []
+correct = []
+omission = []
+ies = []
+swibon = []
+for index, row in df_behavior_grouped.iterrows():
+
+    # Find matching rows
+    df = df_rates[
+        (df_rates["id"] == row["id"])
+        & (df_rates[factors[0]] == row[factors[0]])
+        & (df_rates[factors[1]] == row[factors[1]])
+        & (df_rates[factors[2]] == row[factors[2]])
+    ]
+
+    # Get rates
+    if df[df["log_accuracy"] == 0]["proportion"].to_numpy().size:
+        incorrect.append(df[df["log_accuracy"] == 0]["proportion"].to_numpy()[0])
+    else:
+        incorrect.append(0)
+    if df[df["log_accuracy"] == 1]["proportion"].to_numpy().size:
+        correct.append(df[df["log_accuracy"] == 1]["proportion"].to_numpy()[0])
+    else:
+        correct.append(0)  
+    if df[df["log_accuracy"] == 2]["proportion"].to_numpy().size:
+        omission.append(df[df["log_accuracy"] == 2]["proportion"].to_numpy()[0])
+    else:
+        omission.append(0)
+        
+    if (row["bonustrial"] == 0) & (row["task_switch"] == 0):
+        swibon.append(0)
+    if (row["bonustrial"] == 0) & (row["task_switch"] == 1):
+        swibon.append(1)
+    if (row["bonustrial"] == 1) & (row["task_switch"] == 0):
+        swibon.append(2)
+    if (row["bonustrial"] == 1) & (row["task_switch"] == 1):
+        swibon.append(3)
+
+# Add columns
+df_behavior_grouped["incorrect"] = incorrect 
+df_behavior_grouped["correct"] = correct 
+df_behavior_grouped["omission"] = omission 
+df_behavior_grouped["swibon"] = swibon 
+
+# Add inverse efficiency score
+df_behavior_grouped["ies"] = df_behavior_grouped.apply(lambda row: row["log_rt"] / row["correct"], axis=1)
+
+# Add combined bonus-switch factor
+df_behavior_grouped["ies"] = df_behavior_grouped.apply(lambda row: row["task_switch"] + row["correct"], axis=1)
+
+g = sns.catplot(
+    x=factors[0],
+    y="log_rt",
+    hue=factors[1],
+    col=factors[2],
+    capsize=0.05,
+    palette="tab20",
+    height=6,
+    aspect=0.75,
+    kind="point",
+    data=df_behavior_grouped,
+)
+g.despine(left=True)
+
+g = sns.catplot(
+    x=factors[0],
+    y="correct",
+    hue=factors[2],
+    col=factors[1],
+    capsize=0.05,
+    palette="tab20",
+    height=6,
+    aspect=0.75,
+    kind="point",
+    data=df_behavior_grouped,
+)
+g.despine(left=True)
+
+# Sequential_position x bonus_trial for accuracies
+df_anova = df_behavior_grouped.groupby(
+    ["id"] + factors, as_index=False
+)[["log_rt", "correct", "ies"]].mean()
+
 anova_out = statsmodels.stats.anova.AnovaRM(
-    data=df_anova_rt,
+    data=df_anova,
     depvar="log_rt",
     subject="id",
-    within=["bonustrial", "sequence_position"],
+    within=factors,
 ).fit()
 print(anova_out)
-
-# Draw rt averages
-g = sns.catplot(
-    x="bonustrial",
-    y="log_rt",
-    hue="sequence_position",
-    capsize=0.05,
-    palette="tab20",
-    height=6,
-    aspect=0.75,
-    kind="point",
-    data=df_anova_rt,
-)
-g.despine(left=True)
-
-# ==============================================================================================================
-
-# Create accuracy df
-df_acc = pd.DataFrame(data=data_acc, columns=columns)
-
-# Draw individual accuracies
-g = sns.catplot(
-    x="bonustrial",
-    y="log_accuracy",
-    hue="id",
-    capsize=0.05,
-    palette="tab20",
-    height=6,
-    aspect=0.75,
-    kind="point",
-    data=df_acc,
-)
-g.despine(left=True)
- 
-# Sequential_position x bonus_trial for accuracies
-df_anova_acc = df_acc.groupby(
-    ["id", "bonustrial",  "sequence_position"], as_index=False
-)["log_accuracy"].mean()
 
 anova_out = statsmodels.stats.anova.AnovaRM(
-    data=df_anova_acc,
-    depvar="log_accuracy",
+    data=df_anova,
+    depvar="correct",
     subject="id",
-    within=["bonustrial", "sequence_position"],
+    within=factors,
 ).fit()
 print(anova_out)
 
-# Draw accuracy averages
+anova_out = statsmodels.stats.anova.AnovaRM(
+    data=df_anova,
+    depvar="ies",
+    subject="id",
+    within=factors,
+).fit()
+print(anova_out)
+
+
+# Run LMER
+md = smf.mixedlm("log_rt ~ C(bonustrial) * C(task_switch) * block_nr", df_behavior_grouped, groups=df_behavior_grouped["id"], re_formula="~block_nr")
+mdf = md.fit(method=["lbfgs"])
+print(mdf.summary())
+
+md = smf.mixedlm("correct ~ C(bonustrial) * C(task_switch) * block_nr", df_behavior_grouped, groups=df_behavior_grouped["id"], re_formula="~block_nr")
+mdf = md.fit(method=["lbfgs"])
+print(mdf.summary())
+
+md = smf.mixedlm("ies ~ C(bonustrial) * C(task_switch) * block_nr", df_behavior_grouped, groups=df_behavior_grouped["id"], re_formula="~block_nr")
+mdf = md.fit(method=["lbfgs"])
+print(mdf.summary())
+
+
+# Load theta table
+data_theta = scipy.io.loadmat(os.path.join(path_theta, "theta_table.mat"))["theta_table"]
+
+
+# Create df
+columns_theta = ["id", "tot", "bonus", "switch", "theta1", "theta2"]
+df_theta = pd.DataFrame(data=data_theta, columns=columns_theta)
+
+anova_out = statsmodels.stats.anova.AnovaRM(
+    data=df_theta,
+    depvar="theta1",
+    subject="id",
+    within=["tot", "bonus", "switch"],
+).fit()
+print(anova_out)
+
 g = sns.catplot(
-    x="bonustrial",
-    y="log_accuracy",
-    hue="sequence_position",
+    x="tot",
+    y="theta1",
+    hue="bonus",
+    col="switch",
     capsize=0.05,
     palette="tab20",
     height=6,
     aspect=0.75,
     kind="point",
-    data=df_anova_acc,
+    data=df_theta,
 )
 g.despine(left=True)
+
+
+anova_out = statsmodels.stats.anova.AnovaRM(
+    data=df_theta,
+    depvar="theta2",
+    subject="id",
+    within=["tot", "bonus", "switch"],
+).fit()
+print(anova_out)
+
+g = sns.catplot(
+    x="tot",
+    y="theta2",
+    hue="bonus",
+    col="switch",
+    capsize=0.05,
+    palette="tab20",
+    height=6,
+    aspect=0.75,
+    kind="point",
+    data=df_theta,
+)
+g.despine(left=True)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
--- bocotilt_ged.m
+++ bocotilt_ged.m
@@ -4,25 +4,24 @@ clear all;
 % Path variables
 PATH_EEGLAB      = '/home/plkn/eeglab2022.0/';
 PATH_AUTOCLEANED = '/mnt/data_dump/bocotilt/2_autocleaned/';
+PATH_GED = '/mnt/data_dump/bocotilt/7_ged/';
 
 % Subject list
-subject_list = {'VP09', 'VP17', 'VP25', 'VP10', 'VP11', 'VP13', 'VP14', 'VP15', 'VP16', 'VP18', 'VP19', 'VP20', 'VP21', 'VP22', 'VP23', 'VP08', 'VP24', 'VP26', 'VP27'};
+%subject_list = {'VP09', 'VP17', 'VP25', 'VP10', 'VP11', 'VP13', 'VP14', 'VP15', 'VP16', 'VP18', 'VP19', 'VP20', 'VP21', 'VP22', 'VP23', 'VP08', 'VP24', 'VP26', 'VP27', 'VP28'};
+subject_list = {'VP09', 'VP17', 'VP25', 'VP11', 'VP13', 'VP14', 'VP15', 'VP16', 'VP18', 'VP19', 'VP20', 'VP21', 'VP22', 'VP23', 'VP08', 'VP24', 'VP26', 'VP27'};
 
 % Init eeglab
 addpath(PATH_EEGLAB);
 eeglab;
 
 % SWITCH: Switch parts of script on/off
-to_execute = {'part1'};
+to_execute = {'part3'};
 
-% Part 1: tf-decomp
+% Part 1: Calculate ged
 if ismember('part1', to_execute)
 
     % Loop subjects
     for s = 1 : length(subject_list)
-
-        % !!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-        s = 4;
 
         % participant identifiers
         subject = subject_list{s};
@@ -34,10 +33,21 @@ if ismember('part1', to_execute)
         % To double precision
         eeg_data = double(EEG.data);
 
+        % Exclude trials
+        to_keep = EEG.trialinfo(:, 2) > 4 & EEG.trialinfo(:, 17) == 1;
+        eeg_data = eeg_data(:, :, to_keep);
+        EEG.trialinfo = EEG.trialinfo(to_keep, :);
+        EEG.trials = sum(to_keep);
+
+        % Binarize block info
+        block_binarized = zeros(EEG.trials, 1);
+        block_binarized(EEG.trialinfo(:, 2) > 7) = 1;
+        EEG.trialinfo = [EEG.trialinfo, block_binarized];
+
         % Construct filter
         nyquist = EEG.srate / 2;
         h_pass = 4;
-        l_pass = 8;
+        l_pass = 7;
         transition_width = 0.2;
         filter_order = round(3 * (EEG.srate / h_pass));
         filter_freqs = [0, (1 - transition_width) * h_pass, h_pass, l_pass, (1 + transition_width) * l_pass, nyquist] / nyquist; 
@@ -62,8 +72,8 @@ if ismember('part1', to_execute)
         eeg_data_filtered = eeg_data_filtered(:, crop_idx, :);
 
         % Find indices of time points for S & R selection
-		tidx_S = times >= 100 & times <= 800;
-		tidx_R = times >= 100 & times <= 800;
+		tidx_S = (times >= 200 & times <= 700) | (times >= 1000 & times <= 1500);
+		tidx_R = (times >= 200 & times <= 700) | (times >= 1000 & times <= 1500);
         
         % Init arrays for trial-specific covariance matrices
         covmats_S = zeros(size(eeg_data, 3), size(eeg_data, 1), size(eeg_data, 1));
@@ -74,7 +84,7 @@ if ismember('part1', to_execute)
 
             % Get data for covariance matrices
             data_S = squeeze(eeg_data_filtered(:, tidx_S, trial_idx));
-            data_R = squeeze(eeg_data_filtered(:, tidx_R, trial_idx));
+            data_R = squeeze(eeg_data(:, tidx_R, trial_idx));
 
             % Mean center data
             data_S = bsxfun(@minus, data_S, mean(data_S, 2));
@@ -117,8 +127,8 @@ if ismember('part1', to_execute)
         end
 
         % Select covariance matrices to actually use... (!)
-        S_single_trial = covmats_S_switch;
-        R_single_trial = covmats_R_repeat;
+        S_single_trial = covmats_S;
+        R_single_trial = covmats_R;
 
         % Compute average covariance matrices
         S = squeeze(mean(S_single_trial, 1));
@@ -143,7 +153,7 @@ if ismember('part1', to_execute)
         extreme_eigs = [];
 
         % Permute
-        n_perm = 100;
+        n_perm = 1000;
         for perm = 1 : n_perm
 
             fprintf('Creating 0-hypothesis distribution - step %i/%i\n', perm, n_perm);
@@ -182,10 +192,10 @@ if ismember('part1', to_execute)
         [extreme_eigs, ~] = sort(extreme_eigs);
 
         % Determine threshold (looking for eigs >= thresh...)
-        thresh = extreme_eigs(n_perm / 100 * 5);
+        thresh = extreme_eigs(ceil(n_perm / 100 * 5));
 
         % Iterate components
-        n_comps = 8;
+        n_comps = EEG.nbchan;
         ged_maps = zeros(n_comps, EEG.nbchan);
         ged_time_series = zeros(n_comps, length(EEG.times), EEG.trials);
         ged_time_series_theta = zeros(n_comps, length(times), EEG.trials);
@@ -248,15 +258,22 @@ if ismember('part1', to_execute)
         % 21: position_distractor
         % 22: sequence_position
         % 23: sequence_length
+        % 24: block_binarized
 
         % Getting some nice indices
-        idx_standard = EEG.trialinfo(:, 4) == 1; 
-        idx_bonus = EEG.trialinfo(:, 4) == 0; 
+        idx_beg_std_rep = EEG.trialinfo(:, 24) == 0 & EEG.trialinfo(:, 4) == 0 & EEG.trialinfo(:, 10) == 0;
+        idx_beg_std_swi = EEG.trialinfo(:, 24) == 0 & EEG.trialinfo(:, 4) == 0 & EEG.trialinfo(:, 10) == 1;
+        idx_beg_bon_rep = EEG.trialinfo(:, 24) == 0 & EEG.trialinfo(:, 4) == 1 & EEG.trialinfo(:, 10) == 0;
+        idx_beg_bon_swi = EEG.trialinfo(:, 24) == 0 & EEG.trialinfo(:, 4) == 1 & EEG.trialinfo(:, 10) == 1;
+        idx_end_std_rep = EEG.trialinfo(:, 24) == 1 & EEG.trialinfo(:, 4) == 0 & EEG.trialinfo(:, 10) == 0;
+        idx_end_std_swi = EEG.trialinfo(:, 24) == 1 & EEG.trialinfo(:, 4) == 0 & EEG.trialinfo(:, 10) == 1;
+        idx_end_bon_rep = EEG.trialinfo(:, 24) == 1 & EEG.trialinfo(:, 4) == 1 & EEG.trialinfo(:, 10) == 0;
+        idx_end_bon_swi = EEG.trialinfo(:, 24) == 1 & EEG.trialinfo(:, 4) == 1 & EEG.trialinfo(:, 10) == 1;
 
         % Some viz
-        figure()
-        n_cols = 3;
-        n_rows = n_comps;
+        figure('Visible', 'off'); clf;
+        n_cols = 4;
+        n_rows = 8;
         for cmp = 1 : n_rows
 
             % Is significant?
@@ -283,23 +300,314 @@ if ismember('part1', to_execute)
 
             % Plot component theta time series
             subplot(n_rows, n_cols, fig_idx_offset + 3)
-            pd_standard = mean(squeeze(ged_time_series_theta(cmp, :, idx_standard)), 2);
-            pd_bonus = mean(squeeze(ged_time_series_theta(cmp, :, idx_bonus)), 2);
-            plot(times, pd_standard, 'LineWidth', 1.5)
+            pd_beg_std_rep = mean(squeeze(ged_time_series_theta(cmp, :, idx_beg_std_rep)), 2);
+            pd_beg_std_swi = mean(squeeze(ged_time_series_theta(cmp, :, idx_beg_std_swi)), 2);
+            pd_beg_bon_rep = mean(squeeze(ged_time_series_theta(cmp, :, idx_beg_bon_rep)), 2);
+            pd_beg_bon_swi = mean(squeeze(ged_time_series_theta(cmp, :, idx_beg_bon_swi)), 2);
+
+            plot(times, pd_beg_std_rep, 'LineWidth', 1.5)
             hold on
-            plot(times, pd_bonus, 'LineWidth', 1.5)
+            plot(times, pd_beg_std_swi, 'LineWidth', 1.5)
+            plot(times, pd_beg_bon_rep, 'LineWidth', 1.5)
+            plot(times, pd_beg_bon_swi, 'LineWidth', 1.5)
             xline([0, 800])
-            legend('std', 'bonus')
+
+            subplot(n_rows, n_cols, fig_idx_offset + 4)
+            pd_end_std_rep = mean(squeeze(ged_time_series_theta(cmp, :, idx_end_std_rep)), 2);
+            pd_end_std_swi = mean(squeeze(ged_time_series_theta(cmp, :, idx_end_std_swi)), 2);
+            pd_end_bon_rep = mean(squeeze(ged_time_series_theta(cmp, :, idx_end_bon_rep)), 2);
+            pd_end_bon_swi = mean(squeeze(ged_time_series_theta(cmp, :, idx_end_bon_swi)), 2);
+
+            plot(times, pd_end_std_rep, 'LineWidth', 1.5)
+            hold on
+            plot(times, pd_end_std_swi, 'LineWidth', 1.5)
+            plot(times, pd_end_bon_rep, 'LineWidth', 1.5)
+            plot(times, pd_end_bon_swi, 'LineWidth', 1.5)
+            xline([0, 800])
+
         end
 
-        aa = bb;
+        % Save figure
+        saveas(gcf, [PATH_GED, 'plot_ged_', subject, '.png']);
 
-
-
-
-
-
+        % Save stuff
+        result = struct();
+        result.extreme_eigs = extreme_eigs;
+        result.eig_thresh = thresh;
+        result.trialinfo = EEG.trialinfo;
+        result.ged_time_series = single(ged_time_series);
+        result.ged_maps = ged_maps;
+        result.eeg_data = single(eeg_data);
+        result.times = EEG.times;
+        result.evecs = evecs;
+        result.evals = evals;
+        result.S = S;
+        result.R = R;
+        save([PATH_GED, 'ged_result_', subject, '.mat'], 'result');
 
     end
 
 end % End part1
+
+
+% Part 2: tf-decomp
+if ismember('part2', to_execute)
+
+    % Load info
+    EEG = pop_loadset('filename', [subject_list{1} '_cleaned.set'], 'filepath', PATH_AUTOCLEANED, 'loadmode', 'info');
+
+    % Determine electrode distances based on cartesian coordinates (loosely adopted on elec_distance.m)
+    dists = zeros(EEG.nbchan);
+    cart_coords = [cell2mat({EEG.chanlocs.X})', cell2mat({EEG.chanlocs.Y})', cell2mat({EEG.chanlocs.Z})'];
+    for ch1 = 1 : EEG.nbchan
+        crds1 = cart_coords(ch1, :);
+        len1 = sqrt(sum(crds1 .^ 2));
+        for ch2 = 1 : EEG.nbchan
+            crds2 = cart_coords(ch2, :);
+            len2 = sqrt(sum(crds2 .^ 2));
+            if ch1 == ch2
+                dists(ch1, ch2) = 0;
+            else
+                r = (len1 + len2) / 2; % Estimate sphere radius from len1 and len2
+                theta = acos(dot(crds1, crds2) / (len1 * len2)); % Angle between A & B in radians
+                dists(ch1, ch2) = r * theta; % Arc length = radius * theta
+            end
+        end
+    end
+
+    % Create spatial filter template
+    focuschan = 127;
+    template_topo = dists(focuschan, :) / max(dists(focuschan, :)); % Normalize distances
+    template_topo = ones(size(template_topo)) - template_topo; % Invert
+
+    % Plot spatial filter map template
+    figure('Visible', 'off'); clf;
+    topoplot(template_topo, EEG.chanlocs, 'electrodes', 'off', 'numcontour', 0)
+    title(['template topo'])
+    set(gcf, 'PaperUnits', 'centimeters')
+    set(gcf, 'PaperPosition', [0, 0, 10, 10])
+    saveas(gcf, [PATH_GED, 'spatial_filter_template.png']);
+    
+    % Set complex Morlet wavelet parameters
+    n_frq = 50;
+    frqrange = [2, 25];
+    tfres_range = [400, 100];
+
+    % Create wavelet frequencies and tapering Gaussian widths in temporal domain
+    tf_freqs = linspace(frqrange(1), frqrange(2), n_frq);
+    fwhmTs = logspace(log10(tfres_range(1)), log10(tfres_range(2)), n_frq);
+
+    % Create wavelets
+    wtime = -2 : 1 / EEG.srate : 2;
+    for frq = 1 : length(tf_freqs)
+        cmw(frq, :) = exp(2 * 1i * pi * tf_freqs(frq) .* wtime) .* exp((-4 * log(2) * wtime.^2) ./ (fwhmTs(frq) / 1000)^2);
+        cmw(frq, :) = cmw(frq, :) ./ max(cmw(frq, :));
+    end
+
+    % Init a result struct
+    tf_result = struct();
+    tf_result.template_topo = template_topo;
+    tf_result.tf_freqs = tf_freqs;
+    tf_result.fwhmTs = fwhmTs;
+    tf_result.tf_data = {};
+    tf_result.tf_data_cond_bl = {};
+    tf_result.subjects = {};
+
+    % Loop subjects
+    for s = 1 : length(subject_list)
+
+        % participant identifiers
+        subject = subject_list{s};
+        id = str2num(subject(3 : 4));
+        tf_result.subjects{end + 1} = subject;
+
+        % Load data
+        load([PATH_GED, 'ged_result_', subject, '.mat']);
+
+        % Threshold eigenvalues
+		thresh_eval_sd = 1; % In sd
+		thresh_eigenvalue = median(result.evals) + std(result.evals) * thresh_eval_sd;
+		suprathresh_eval_idx = find(result.evals > thresh_eigenvalue);  
+
+        % Find highest similarity in supra-threshold non-blink cmps
+		cmpsim = 0;
+		chosen_cmp = 0;
+		for cmp = 1 : EEG.nbchan
+			if ismember(cmp, suprathresh_eval_idx)
+				tmp_cmp = result.ged_maps(cmp, :) / max(result.ged_maps(cmp, :)); % Normalize
+				tmp = abs(corrcoef(tmp_cmp, template_topo));
+				if tmp(1, 2) * result.evals(cmp) > cmpsim
+					cmpsim = tmp(1, 2) * result.evals(cmp);
+					chosen_cmp = cmp;
+				end
+			end	
+		end
+
+        % Save filter topography
+		figure('Visible', 'off'); clf;
+		topoplot(result.ged_maps(chosen_cmp, :), EEG.chanlocs, 'electrodes', 'off', 'numcontour', 0)
+        saveas(gcf, [PATH_GED, 'spatial_filter_topo_' subject '.png']);
+        tf_result.cmp_topo = result.ged_maps(chosen_cmp, :);
+
+        % Get component signal
+        cmp_time_series = squeeze(result.ged_time_series (chosen_cmp, :, :));
+
+        % tf decomp of component
+        convlen = size(cmp_time_series, 1) * size(cmp_time_series, 2) + size(cmw, 2) - 1;
+
+        % cmw to freq domain and scale
+        cmwX = zeros(length(tf_freqs), convlen);
+        for f = 1 : length(tf_freqs)
+            cmwX(f, :) = fft(cmw(f, :), convlen);
+            cmwX(f, :) = cmwX(f, :) ./ max(cmwX(f, :));
+        end
+
+        % Get TF-power
+        powcube = NaN(length(tf_freqs), size(cmp_time_series, 1), size(cmp_time_series, 2));
+        tmp = fft(reshape(double(cmp_time_series), 1, []), convlen);
+        for f = 1 : length(tf_freqs)
+            as = ifft(cmwX(f, :) .* tmp); 
+            as = as(((size(cmw, 2) - 1) / 2) + 1 : end - ((size(cmw, 2) - 1) / 2));
+            as = reshape(as, size(cmp_time_series, 1), size(cmp_time_series, 2));
+            powcube(f, :, :) = abs(as) .^ 2;          
+        end
+
+        % Cut edges
+        powcube = powcube(:, dsearchn(result.times', -500) : dsearchn(result.times', 2000), :);
+        prune_time = result.times(dsearchn(result.times', -500) : dsearchn(result.times', 2000));
+        tf_result.tf_time = prune_time;
+
+        % Get condition general baseline values
+        bl_idx = prune_time >= -500 & prune_time <= -200;
+
+        % Calc blvals
+        tmp = squeeze(mean(powcube, 3));
+        blvals = squeeze(mean(tmp(:, bl_idx), 2));
+
+        % Calculate ersp for conditions
+        ersps = zeros(2, 2, 2, size(powcube, 1), size(powcube, 2));
+        ersps_cond_bl = zeros(2, 2, 2, size(powcube, 1), size(powcube, 2));
+        for tot = 1 : 2
+            for bon = 1 : 2
+                for swi = 1 : 2
+
+                    % Get trial idx
+                    trial_idx = result.trialinfo(:, 24) == tot - 1 & result.trialinfo(:, 4) == bon - 1 & result.trialinfo(:, 10) == swi - 1;
+
+                    % Get condition mean
+                    condition_mean = squeeze(mean(powcube(:, :, trial_idx), 3));
+
+                    % Get ersp
+                    ersps(tot, bon, swi, :, :) = 10 * log10(bsxfun(@rdivide, condition_mean, blvals));
+
+                    % Get condition specific baseline ersp
+                    blvals_condition = squeeze(mean(condition_mean(:, bl_idx), 2));
+                    ersps_cond_bl(tot, bon, swi, :, :) = 10 * log10(bsxfun(@rdivide, condition_mean, blvals_condition));
+  
+                end
+            end
+        end
+
+        % Copy to results
+        tf_result.tf_data{end + 1} = ersps;
+        tf_result.tf_data_cond_bl{end + 1} = ersps_cond_bl;
+
+    end % End subject loop
+
+    % Save tf results
+    save([PATH_GED, 'tf_results.mat'], 'tf_result');
+
+end % End part2
+
+% Part 3: Analysis
+if ismember('part3', to_execute)
+
+    % Load tf results
+    load([PATH_GED, 'tf_results.mat']);
+
+    % Concatenate tf data. Dims: tot, bonus, switch, freqs, times, subjects
+    tf_data = cat(6, tf_result.tf_data_cond_bl{:});
+
+    % Matrix for theta traces
+    theta_traces = [];
+
+    % Plot ersp and calculate theta traces
+    figure()
+    counter = 0;
+    for tot = 1 : 2
+        for bon = 1 : 2
+            for swi = 1 : 2
+
+                counter = counter + 1;
+
+                % Plot data
+                pd = squeeze(mean(squeeze(tf_data(tot, bon, swi, :, :, :)), 3));
+
+                subplot(2, 4, counter)
+                contourf(tf_result.tf_time, tf_result.tf_freqs, pd, 40, 'linecolor','none')
+                colormap('jet')
+                set(gca, 'clim', [-3, 3], 'YScale', 'lin', 'YTick', [4, 8, 12, 20])
+
+         
+                titlestring = ['tot: ', num2str(tot), ' - bon: ', num2str(bon), ' - swi: ', num2str(swi)];
+
+                title(titlestring, 'FontSize', 10);
+
+                % get theta_trace
+                theta_idx = tf_result.tf_freqs >= 4 & tf_result.tf_freqs <= 8;
+                theta_traces(counter, :) = mean(pd(theta_idx, :), 1);
+            end
+        end
+    end
+
+    % Save theta traces
+    dlmwrite([PATH_GED, 'theta_traces.csv'], theta_traces);
+    dlmwrite([PATH_GED, 'theta_traces_times.csv'], tf_result.tf_time);
+
+    % Calculate the grand-average theta-time-series
+    grand_average_theta = mean(theta_traces, 1);
+
+    % Pop up plot of peaks in the theta grand-average
+    figure()
+    findpeaks(grand_average_theta)
+
+    % Save peak indices.
+    [~, theta_peak_indices] = findpeaks(grand_average_theta);
+
+    % Define width of the time window 
+    winwidth = 200;
+
+    % Determine time-windows aroud these peaks
+    theta_window_1 = [tf_result.tf_time(theta_peak_indices(1)) - winwidth / 2, tf_result.tf_time(theta_peak_indices(1)) + winwidth / 2];
+    theta_window_2 = [tf_result.tf_time(theta_peak_indices(2)) - winwidth / 2, tf_result.tf_time(theta_peak_indices(2)) + winwidth / 2]; 
+
+    % Iterate subjects and conditions again and parameterize theta
+    theta_table = [];
+    counter = 0;
+    for s = 1 : length(subject_list)
+        for tot = 1 : 2
+            for bon = 1 : 2
+                for swi = 1 : 2
+        
+                    counter = counter + 1;
+
+                    % Get time idx
+                    time_idx_win_1 = tf_result.tf_time >= theta_window_1(1) & tf_result.tf_time <= theta_window_1(2);
+                    time_idx_win_2 = tf_result.tf_time >= theta_window_2(1) & tf_result.tf_time <= theta_window_2(2);
+
+                    % get averages
+                    th1 = mean2(tf_data(tot, bon, swi, theta_idx, time_idx_win_1, s));
+                    th2 = mean2(tf_data(tot, bon, swi, theta_idx, time_idx_win_2, s));
+
+                    % Collect
+                    theta_table(counter, :) = [s, tot, bon, swi, th1, th2];
+
+                end
+            end
+        end
+    end
+
+    save([PATH_GED, 'theta_table.mat'], 'theta_table');
+
+
+end % End part3
